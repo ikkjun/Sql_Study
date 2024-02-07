f0e0184456889673a17540ef3c0e0fe3983951d5
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-02-07 19:56:59 +0900

    Update Creating_Databases_Tables.sql

--- Creating_Databases_Tables.sql
+++ Creating_Databases_Tables.sql
@@ -3,3 +3,30 @@ show databases;
 create database slimestore;
 create database nikestore;
 drop database slimestore;
+
+use nikestore;
+
+create database CAT;
+use CAT;
+create table cats
+(
+name varchar(10),
+age int
+);
+insert into cats(name, age) values('페르시안', 5);
+
+select * from cats;
+
+show tables;
+
+show columns from cats;
+
+desc cats; -- describe cats
+
+create table dogs
+(
+breed varchar(10),
+age int(10)
+);
+
+drop table dogs;
